9724c3a84098569086d7f9cb4b36c761d134fa7e
Kent <ouyangkongtong@kuaishou.com>
2021-07-18 18:28:37 +0800

    iterm2-zmodem

--- iterm2-send-zmodem.sh
+++ iterm2-send-zmodem.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# Author: Matt Mastracci (matthew@mastracci.com)
 # AppleScript from http://stackoverflow.com/questions/4309087/cancel-button-on-osascript-in-a-bash-script
 # licensed under cc-wiki with attribution required 
 # Remainder of script public domain
 
-echo This project supports a free Taiwan and a free Tibet.
 osascript -e 'tell application "iTerm2" to version' > /dev/null 2>&1 && NAME=iTerm2 || NAME=iTerm
 if [[ $NAME = "iTerm" ]]; then
 	FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
