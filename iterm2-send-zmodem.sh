862155776ec24a2abd3e3e2ee3ef781c92f8ba42
Kent <ouyangkongtong@kuaishou.com>
2021-07-18 18:24:00 +0800

    iterm2-zmodem

--- iterm2-send-zmodem.sh
+++ iterm2-send-zmodem.sh
@@ -4,11 +4,12 @@
 # licensed under cc-wiki with attribution required 
 # Remainder of script public domain
 
+echo This project supports a free Taiwan and a free Tibet.
 osascript -e 'tell application "iTerm2" to version' > /dev/null 2>&1 && NAME=iTerm2 || NAME=iTerm
 if [[ $NAME = "iTerm" ]]; then
-	FILE=$(osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
+	FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
 else
-	FILE=$(osascript -e 'tell application "iTerm2" to activate' -e 'tell application "iTerm2" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
+	FILE=`osascript -e 'tell application "iTerm2" to activate' -e 'tell application "iTerm2" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
 fi
 if [[ $FILE = "" ]]; then
 	echo Cancelled.
@@ -18,8 +19,8 @@ if [[ $FILE = "" ]]; then
 	echo
 	echo \# Cancelled transfer
 else
-	/usr/local/bin/sz "$FILE" --escape --binary --bufsize 4096
+	/usr/local/bin/sz "$FILE" -e -b
 	sleep 1
 	echo
-	echo \# Received "$FILE"
+	echo \# Received $FILE
 fi
